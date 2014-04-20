0c2f763b45f92aba491862699b2c52240c4ba53c
Ahriman <seal789ie@gmail.com>
2014-04-20 23:21:28 +0800

    Commited Test

---
+++ soldier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Side_scrolling_Tower_Defense
+{
+    class soldier
+    {
+        //Property
+
+        //Construct
+
+        //Method
+    }
+}
